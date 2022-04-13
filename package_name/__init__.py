acadd477cfca5acb12a3ccd135522345444d3dbb
Alberto Ferrario <albertogferrario@gmail.com>
2022-04-13 02:04:26 +0200

    Fix versioning

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,3 +1,7 @@
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('tornado-jsonschema').version
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+__version__ = '0.1.0'
