480b6bb265db951a931d034e058c6e01b0c75048
Alberto Giancarlo Ferrario <me@albertogferrario.com>
2022-04-22 23:38:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/webfucktory/package-name",
-    packages=find_packages(),
+    packages=['package_name'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
